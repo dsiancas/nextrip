ca0c8e91b4d9e4ccc703e745102f845264fd34b9
danielucsp <daniel.salinas@ucsp.edu.pe>
2014-06-16 15:55:15 -0500

    para que funcione location y el home

--- location/models.py
+++ location/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Location(models.Model):
 	id_location = models.IntegerField(primary_key=True)
-	name = models.CharField(max_length=250)
+	nombre = models.CharField(max_length=250)
 	description = models.CharField(max_length=650)
 	
 	def __unicode__(self):
